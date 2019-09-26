8aa968960de63e3643840355c062510a1eaeb473
wanghao <wanghao@viewhigh.com>
2019-09-26 14:22:57 +0800

    String equal method

---
+++ utils/src/main/java/com/peterwanghao/samples/java/utils/StringUtil.java
@@ -0,0 +1,63 @@
+package com.peterwanghao.samples.java.utils;
+
+/**
+ * @ClassName: StringUtil
+ * @Description:TODO(这里用一句话描述这个类的作用)
+ * @author: wanghao
+ * @date: 2019年9月26日 下午1:57:53
+ * @version V1.0
+ * 
+ */
+public class StringUtil {
+
+	public static void equal1(String i, String j) {
+		try {
+			if (i == j) {
+				System.out.println("equal1相等");
+			}
+		} catch (Exception ex) {
+			System.out.println("equal1 异常：");
+			ex.printStackTrace();
+		} finally {
+			System.out.println("equal1结束");
+		}
+
+	}
+
+	public static void equal2(String i, String j) {
+		try {
+			if(i.equals(j)) {
+				System.out.println("equal2相等");
+			}
+		} catch (Exception ex) {
+			System.out.println("equal2 异常：");
+			ex.printStackTrace();
+		} finally {
+			System.out.println("equal2结束");
+		}
+	}
+
+	public static void equal3(String i, String j) {
+		try {
+			if(j.equals(i)) {
+				System.out.println("equal3相等");
+			}
+			if (i instanceof String) {
+				System.out.println("is a String");
+			}else {
+				System.out.println("is Not String");
+			}
+		} catch (Exception ex) {
+			System.out.println("equal3异常：");
+			ex.printStackTrace();
+		} finally {
+			System.out.println("equal3结束");
+		}
+	}
+	
+	public static void main(String[] args) {
+		equal1(null,"abc");
+		equal2(null,"abc");
+		equal3(null,"abc");
+    }
+}
